ce2eabe4f5d1a62022009ebaf43fb415acaff0f7
Danil <t902hda@mail.ru>
2025-01-12 20:56:36 +0300

    refactoring: add zero before date fn

--- src/utils/addZeroBeforeDate.ts
+++ src/utils/addZeroBeforeDate.ts
@@ -1,4 +1,3 @@
 export const addZeroBeforeDate = (date: number) => {
-    return String(date).padStart(2, '0')
-    // return date < 10 ? '0' + date : date
+    return date < 10 ? '0' + date : date
 }
